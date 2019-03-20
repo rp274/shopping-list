7aed6cb9ad0b5d8d780ab31392b17aeb15f46c8e
Ryan Prescott <ryanprsct91@gmail.com>
2019-03-20 12:36:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 $(function(){
+//adding items via form  
   $('#js-shopping-list-form').submit(function(event) {
+//stop default form action    
     event.preventDefault();
 
     const listItem = $('.js-shopping-list-entry').val();
-
+//clear form input
     $('.js-shopping-list-entry').val('');
-
+//add new item
     $('.shopping-list').append(
       `<li>
         <span class="shopping-item">${listItem}</span>
@@ -20,13 +22,13 @@ $(function(){
       </li>`);
   });
 
-
+//toggle strike through on item
 $('.shopping-list').on('click', '.shopping-item-toggle',
   function(event) {
     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
   });
 
-
+//delete item from list
 $('.shopping-list').on('click', '.shopping-item-delete',
   function(event) {
     $(this).closest('li').remove();
